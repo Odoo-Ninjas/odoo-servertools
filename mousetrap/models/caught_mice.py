d18cdd45cd7a0cb712e3b277f93757caa47d9c37
Marc Wimmer <marc@itewimmer.de>
2024-02-14 14:47:12 +0100

    sorting order

--- mousetrap/models/caught_mice.py
+++ mousetrap/models/caught_mice.py
@@ -4,6 +4,7 @@ from odoo.exceptions import UserError, RedirectWarning, ValidationError
 
 class Caught(models.Model):
     _name = "mousetrap.catches"
+    _order = "id desc"
 
     excinfo = fields.Char("Exception")
     stacktrace = fields.Text("Stack Trace")
